81462d5965861ac2cf808fd6da579d13a3b0f05e
HaopingZheng <haoping.zheng@ucalgary.ca>
2022-07-18 14:41:28 -0600

    Added my branch label

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Alan's label"));
+			root.getChildren().add(new Label("Ryan's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Ryan's project");
